6847cc7897c0b8b9079f148a9f23b164fbd53fce
Reese <charisearter.dev@gmail.com>
2024-05-24 13:33:44 -0400

    initial client side setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+function App() {
+	return (
+		<Fragment>
+			<div className='container'>
+				<h1>ToDo App Tester: Initial</h1>
+			</div>
+		</Fragment>
+	);
+}
+
+export default App;
